ee47f0c501a749ce0058edc8ac9615eafe776373
Sean Callahan <seanahan5000@gmail.com>
2025-01-10 10:41:39 -0600

    Snapshot

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,6 +3,8 @@ import * as vscode from 'vscode'
 import * as path from 'path'
 import * as vsclnt from 'vscode-languageclient'
 import * as cmd from "./commands"
+// import { RpwDebugSession } from "./rpwDebug"
+import { activateMockDebug } from './activateMockDebug';
 
 import {
 	LanguageClient,
@@ -15,6 +17,7 @@ let client: LanguageClient
 let statusBarItem: vscode.StatusBarItem
 
 export function activate(context: vscode.ExtensionContext) {
+
 	// The server is implemented in node
 	const serverModule = context.asAbsolutePath(
 		path.join('server', 'out', 'server.js')
@@ -56,6 +59,42 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(() => updateStatusItem()))
 
 	client.onNotification("rpw.syntaxChanged", () => { updateStatusItem() })
+
+	// const provider = new RpwConfigurationProvider()
+	// context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('rpw65', provider))
+
+	// const factory = new InlineDebugAdapterFactory();
+	// context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('rpw65', factory));
+	// if ('dispose' in factory) {
+	// 	context.subscriptions.push(factory)
+	// }
+
+	// context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('rpw65', {
+	// 	provideDebugConfigurations(folder: vscode.WorkspaceFolder | undefined): vscode.ProviderResult<vscode.DebugConfiguration[]> {
+	// 		return [
+	// 			{
+	// 				name: "Dynamic Launch",
+	// 				request: "launch",
+	// 				type: "rpw65",
+	// 				program: "${file}"
+	// 			},
+	// 			{
+	// 				name: "Another Dynamic Launch",
+	// 				request: "launch",
+	// 				type: "rpw65",
+	// 				program: "${file}"
+	// 			},
+	// 			{
+	// 				name: "Mock Launch",
+	// 				request: "launch",
+	// 				type: "rpw65",
+	// 				program: "${file}"
+	// 			}
+	// 		];
+	// 	}
+	// }, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
+
+	activateMockDebug(context)
 }
 
 export function deactivate(): Thenable<void> | undefined {
@@ -103,3 +142,41 @@ async function updateStatusItem() {
 		statusBarItem.hide()
 	}
 }
+
+
+// class RpwConfigurationProvider implements vscode.DebugConfigurationProvider {
+
+// 	/**
+// 	 * Massage a debug configuration just before a debug session is being launched,
+// 	 * e.g. add all missing attributes to the debug configuration.
+// 	 */
+// 	resolveDebugConfiguration(folder: vscode.WorkspaceFolder | undefined, config: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
+
+// 		// if launch.json is missing or empty
+// 		if (!config.type && !config.request && !config.name) {
+// 			const editor = vscode.window.activeTextEditor;
+// 			if (editor && editor.document.languageId === 'rpw65') {
+// 				config.type = 'rpw65';
+// 				config.name = 'Launch';
+// 				config.request = 'launch';
+// 				config.program = '${file}';
+// 				config.stopOnEntry = true;
+// 			}
+// 		}
+
+// 		if (!config.program) {
+// 			return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
+// 				return undefined;	// abort launch
+// 			});
+// 		}
+
+// 		return config;
+// 	}
+// }
+
+// class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
+
+// 	createDebugAdapterDescriptor(_session: vscode.DebugSession): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+// 		return new vscode.DebugAdapterInlineImplementation(new RpwDebugSession());
+// 	}
+// }
